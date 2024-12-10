cd5ead0ca9e8a32ce6f2a48089a6b799bfb88885
Manish Tewatia <ManishTewatia3020@gmail.com>
2024-12-10 15:29:02 +0530

    Add various images into the modules

--- src/Menu.js
+++ src/Menu.js
@@ -3,7 +3,14 @@ import { Heading , MenuOrder } from "./Home";
 
 import { ContactDetails } from "./ContactInfo.js";
 
+import Panner from "./paneer.jpg"
+
+import Bread from "./hq720.jpg"
+
+import aalo from "./aloo-gobi.jpg"
+
 export let MenuDetails;
+
 
 export function MenuOrderDetail(){
 
@@ -11,18 +18,25 @@ console.log(`Menu-Tab`)
 
 MenuDetails = document.createElement("div");
 
-MenuDetails.innerHTML = `<div>Food number 1</div><div>Food number 1</div><div>Food number 1</div><div>Food number 1</div><div>Food number 1</div><div>Food number 1</div>`
+MenuDetails.innerHTML = `<div class="First-Food">
+<img class="food-photos" src="${Panner}">
+Shahi Paneer
+</div>
+<div>
+<img class="food-photos" src="${Bread}">
+Bread Omelette
+</div>
+<div>
+<img class="food-photos" src="${aalo}">
+Aalo gobhi ki sabji
+</div>`
 
 MenuDetails.classList.add(`menu-details`)
 
-
-
 console.log(MenuDetails)
 document.getElementById(`content`).appendChild(MenuDetails)
-
 if(document.getElementById(`content`).contains(Heading)){
 document.getElementById(`content`).removeChild(Heading)}
-
 
 if(document.getElementById(`content`).contains(MenuOrder)){
 document.getElementById(`content`).removeChild(MenuOrder)}
